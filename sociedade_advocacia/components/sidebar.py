ba6edfaaf5ef5988b33321c3892d57c24f42e25f
eltoncox <elton.unix@gmail.com>
2026-02-07 20:20:35 -0300

    sidebar

--- sociedade_advocacia/components/sidebar.py
+++ sociedade_advocacia/components/sidebar.py
@@ -10,12 +10,49 @@ from components import modal_novo_processo, modal_novo_advogado, modal_advogados
 from app import app
 
 # ========= Layout ========= #
-layout = dbc.Container([])
+layout = dbc.Container([
+        modal_novo_processo.layout,
+        modal_novo_advogado.layout,
+        modal_advogados.layout,
+        dbc.Container([
+            dbc.Row([
+                dbc.Col([
+                    html.H1("ASIMOV", style={'color': 'yellow'})#, 'font-size': 'width / 1.2'})
+                ])
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    html.H3("ASSOCIATES", style={'color': 'white'})
+                ]),
+            ]),
+        ], style={'padding-top': '50px', 'margin-bottom': '100px'}, className='text-center'),
+        html.Hr(),
+        dbc.Row([
+            dbc.Col([
+                dbc.Nav([
+                    dbc.NavItem(dbc.NavLink([html.I(className='fa fa-home dbc'), "\tINÍCIO"], href="/home", active=True, style={'text-align': 'left'})),
+                    html.Br(),
+                    dbc.NavItem(dbc.NavLink([html.I(className='fa fa-plus-circle dbc'), "\tPROCESSOS"], id='processo_button', active=True, style={'text-align': 'left'})),
+                    html.Br(),
+                    dbc.NavItem(dbc.NavLink([html.I(className='fa fa-user-plus dbc'), "\tADVOGADOS"], id='lawyers_button', active=True, style={'text-align': 'left'})),
+                ], vertical="lg", pills=True, fill=True)
+            ])
+        ]),
+], style={'height': '100vh', 'padding': '0px', 'position':'sticky', 'top': 0, 'background-color': '#232423'})
     
 
 
 # ======= Callbacks ======== #
 # Abrir Modal New Lawyer
-
+@app.callback(
+    Output('modal_new_lawyer', "is_open"),
+    Input('new_adv_button', 'n_clicks'),
+    Input("cancel_button_novo_advogado", 'n_clicks'),
+    State('modal_new_lawyer', "is_open")
+)
+def toggle_modal(n, n2, is_open):
+    if n or n2:
+        return not is_open
+    return is_open
 
 # Abrir Modal Lawyers
