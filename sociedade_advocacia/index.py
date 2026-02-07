ea28c82d934abecc895df7c9f18585de8ef9c452
eltoncox <elton.unix@gmail.com>
2026-02-07 19:59:08 -0300

    index

--- sociedade_advocacia/index.py
+++ sociedade_advocacia/index.py
@@ -15,21 +15,52 @@ from components import home, sidebar
 # =========  Layout  =========== #
 app.layout = dbc.Container([
     # Store e Location 
-    
+    dcc.Location(id="url"), # url que estou acessando
+    dcc.Store(id='store_intermedio', data={}),
+    dcc.Store(id='store_adv', data={}),
+    dcc.Store(id='store_proc', data={}),
+    html.Div(id='div_fantasma'),
 
     # Layout
-    
+    dbc.Row([
+        dbc.Col([
+            sidebar.layout
+        ], md=2, style={'padding': '0px'}),
 
+        dbc.Col([
+            dbc.Container(id="page-content", fluid=True, style={'height': '100%', 'width': '100%', 'padding-left': '14px'}) 
+        ], md=10, style={'padding': '0px'}),
+    ])
 ], fluid=True)
 
 
 
 # ======= Callbacks ======== #
 # URL callback to update page content
-
+@app.callback(Output('page-content', 'children'), Input('url', 'pathname'))
+def render_page(pathname):
+    if pathname == '/home' or pathname == '/':
+        return home.layout
+    else: 
+       return dbc.Container([
+            html.h1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"O caminho  '{pathname}' não foi reconhecido..."),
+            html.p("Use a NavBar para retornar ao sistema de maneira correta.")
+        ])
 
 # Dcc.Store back to file
+@app.callback(
+    Output('div_fantasma', 'children'),
+    Input('store_adv', 'data'),
+    Input('store_proc', 'data'),
+)
+def update_file(adv_data, proc_data):
+    df_adv_aux = pd.DataFrame(adv_data)
+    df_proc_aux = pd.DataFrame(proc_data)
+
+    return []
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
